26fc48ce6be00b18c428edfd5589b33fe4a8e3e2
Tony Lukasavage <anthony.lukasavage@gmail.com>
2022-02-02 10:16:12 -0500

    reorg

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { Layout, Menu } from 'antd';
+import {
+  UserOutlined,
+  UploadOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import './App.css';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+class App extends React.Component {
+	render() {
+		return <Layout hasSider>
+		<Sider
+		  style={{
+			overflow: 'auto',
+			height: '100vh',
+			position: 'fixed',
+			left: 0,
+			top: 0,
+			bottom: 0,
+		  }}
+		>
+		  <div className="logo" />
+		  <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+			<Menu.Item key="1" icon={<UserOutlined />}>
+			  nav 1
+			</Menu.Item>
+			<Menu.Item key="2" icon={<VideoCameraOutlined />}>
+			  nav 2
+			</Menu.Item>
+			<Menu.Item key="3" icon={<UploadOutlined />}>
+			  nav 3
+			</Menu.Item>
+		  </Menu>
+		</Sider>
+		<Layout className="site-layout" style={{ marginLeft: 200 }}>
+		  <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+			<div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
+			  content
+			</div>
+		  </Content>
+		</Layout>
+	  </Layout>;
+	}
+}
+
+export default App;
--- src/renderer.js
+++ src/renderer.js
@@ -29,10 +29,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
-
-import App from './App';
-import './index.css';
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
